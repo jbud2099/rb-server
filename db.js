54a80de6645b109cf18718ebe173f2dcbd90bb49
jbud2099 <jbud2099@gmail.com>
2021-05-25 19:19:28 -0400

    changed .env

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize').Sequelize
 
-let sequelize = new Sequelize(process.env.DB_CONN_URL || 
+let sequelize = new Sequelize(process.env.DATABASE_URL || 
     `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/videogame-review`, 
     {
     dialect: 'postgres',
